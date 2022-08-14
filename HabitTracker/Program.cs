56b2513ea6f4e43c95354d087a55aae894d7150a
Brandon <brandon.ebarnett@gmail.com>
2022-08-14 13:39:47 -0400

    Initial commit

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -1,8 +1,1 @@
-﻿using Microsoft.Data.Sqlite;
-
-string connectionString = @"Data Source=habit-tracker.db";
-
-using (SqliteConnection connection = new SqliteConnection(connectionString))
-{
-
-}
+﻿
